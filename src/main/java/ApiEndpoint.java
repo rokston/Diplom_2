7c3629c1d90e9c63de310718b325b8f716efcc76
Daria Dubkova <rokston@mail.ru>
2023-11-01 23:27:54 +0300

    3rd code commit

--- src/main/java/ApiEndpoint.java
+++ src/main/java/ApiEndpoint.java
@@ -4,6 +4,6 @@ public class ApiEndpoint {
     public static final String LOGIN_USER = "/api/auth/login";
     public static final String DELETE_USER = "/api/auth/user";
     public static final String CHANGE_USER_DATA = "/api/auth/user";
-   // public static final String CREATE_ORDER = "/api/v1/orders";
+    public static final String CREATE_ORDER = "/api/orders";
   //  public static final String GET_ORDER_LIST = "/api/v1/orders";
 }
